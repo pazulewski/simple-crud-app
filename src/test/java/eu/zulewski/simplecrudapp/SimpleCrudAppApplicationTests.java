db738f577ec0d393012c11ef15d403d6ee085814
Paweł Żulewski <pazulewski@gmail.com>
2021-12-08 17:24:59 +0100

    Initial commit

---
+++ src/test/java/eu/zulewski/simplecrudapp/SimpleCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.zulewski.simplecrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
