0097fbf9b7ba49a69c88f481b7bd8e454d3ceb69
Paweł Żulewski <pazulewski@gmail.com>
2021-12-08 17:45:26 +0100

    add EmployeeService & EmployeeServiceImpl

---
+++ src/main/java/eu/zulewski/simplecrudapp/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package eu.zulewski.simplecrudapp.service;
+
+import eu.zulewski.simplecrudapp.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getAll();
+}
---
+++ src/main/java/eu/zulewski/simplecrudapp/service/EmployeeServiceImpl.java
@@ -0,0 +1,20 @@
+package eu.zulewski.simplecrudapp.service;
+
+import eu.zulewski.simplecrudapp.entity.Employee;
+import eu.zulewski.simplecrudapp.repository.EmployeeRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> getAll() {
+        return employeeRepository.findAll();
+    }
+}
